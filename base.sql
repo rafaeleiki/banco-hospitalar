441c5ee9237244e985444207ee200c1c97d8d130
Rafael <rafaeleiki@yahoo.com.br>
2017-05-05 01:16:02 -0300

    Atualizado os esquemas normalizados

--- base.sql
+++ base.sql
@@ -39,57 +39,80 @@ CREATE TABLE enfermeiro (
 );
 
 CREATE TABLE consulta (
-  crm varchar(20) REFERENCES medico(crm),
-  cpf varchar(11) REFERENCES paciente(cpf),
+  cpf_medico varchar(11) REFERENCES medico(cpf),
+  cpf_paciente varchar(11) REFERENCES paciente(cpf),
   data_consulta timestamp WITH TIME ZONE,
-  tipo text,
-  PRIMARY KEY(crm, cpf, data_consulta)
+  tipo text NOT NULL,
+  PRIMARY KEY(cpf_medico, cpf_paciente, data_consulta)
 );
 
 CREATE TABLE anamnese (
-  crm varchar(20),
-  cpf varchar(11),
+  cpf_medico varchar(11),
+  cpf_paciente varchar(11),
   data_consulta timestamp WITH TIME ZONE,
-  pressao integer,
-  temperatura integer,
-  batimentos integer,
-  queixas text,
+  pressao integer NOT NULL,
+  temperatura integer NOT NULL,
+  batimentos integer NOT NULL,
+  queixas text NOT NULL,
   comentarios text,
   hipoteses text,
   recomendacoes text,
-  FOREIGN KEY(crm, cpf, data_consulta) REFERENCES consulta(crm, cpf, data_consulta),
-  PRIMARY KEY(crm, cpf, data_consulta)
+  FOREIGN KEY(cpf_medico, cpf_paciente, data_consulta)
+    REFERENCES consulta(cpf_medico, cpf_paciente, data_consulta),
+  PRIMARY KEY(cpf_medico, cpf_paciente, data_consulta)
 );
 
 CREATE TABLE enfermidade (
   cod_enfermidade integer PRIMARY KEY,
-  nome_enfermidade varchar(50) NOT NULL,
-  descricao_enfermidade text
+  nome_enfermidade varchar(50) NOT NULL
+);
+
+CREATE TABLE descricao_enfermidade (
+  cod_enfermidade integer PRIMARY KEY REFERENCES enfermidade(cod_enfermidade),
+  descricao text NOT NULL
 );
 
 CREATE TABLE remedio (
   cod_remedio integer PRIMARY KEY,
-  nome_remedio varchar(50) NOT NULL,
+  nome_remedio varchar(50) NOT NULL
+);
+
+CREATE TABLE descricao_remedio (
+  cod_remedio integer PRIMARY KEY REFERENCES remedio(cod_remedio),
   indicacao text,
   dosagem_maxima varchar(50)
 );
 
 CREATE TABLE internacao (
-  crm varchar(20),
-  cpf varchar(11),
+  cpf_medico varchar(11),
+  cpf_paciente varchar(11),
   data_consulta timestamp WITH TIME ZONE,
+  data_entrada timestamp WITH TIME ZONE,
+  FOREIGN KEY(cpf_medico, cpf_paciente, data_consulta)
+    REFERENCES consulta(cpf_medico, cpf_paciente, data_consulta),
+  PRIMARY KEY(cpf_medico, cpf_paciente, data_consulta, data_entrada)
+);
+
+CREATE TABLE descricao_internacao (
+  cpf_medico varchar(11),
+  cpf_paciente varchar(11),
   data_entrada timestamp WITH TIME ZONE,
   data_saida timestamp WITH TIME ZONE,
   leito varchar(50) NOT NULL,
-  FOREIGN KEY(crm, cpf, data_consulta) REFERENCES consulta(crm, cpf, data_consulta),
-  PRIMARY KEY(crm, cpf, data_consulta, data_entrada)
+  FOREIGN KEY(cpf_medico, cpf_paciente, data_consulta)
+    REFERENCES consulta(cpf_medico, cpf_paciente, data_consulta),
+  PRIMARY KEY(cpf_medico, cpf_paciente, data_entrada)
 );
 
 CREATE TABLE procedimento (
   cod_procedimento integer PRIMARY KEY,
   nome_procedimento varchar(50) NOT NULL,
-  data_procedimento timestamp WITH TIME ZONE NOT NULL,
-  descricao_procedimento text
+  data_procedimento timestamp WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE descricao_procedimento (
+  cod_procedimento integer PRIMARY KEY REFERENCES procedimento(cod_procedimento),
+  descricao text NOT NULL
 );
 
 CREATE TABLE exame (
@@ -103,30 +126,33 @@ CREATE TABLE cirurgia (
 );
 
 CREATE TABLE anamnese_enfermidade (
-  crm varchar(20),
-  cpf varchar(11),
+  cpf_medico varchar(11),
+  cpf_paciente varchar(11),
   data_consulta timestamp WITH TIME ZONE,
   cod_enfermidade integer REFERENCES enfermidade(cod_enfermidade),
-  FOREIGN KEY(crm, cpf, data_consulta) REFERENCES consulta(crm, cpf, data_consulta),
-  PRIMARY KEY(crm, cpf, data_consulta, cod_enfermidade)
+  FOREIGN KEY(cpf_medico, cpf_paciente, data_consulta)
+    REFERENCES consulta(cpf_medico, cpf_paciente, data_consulta),
+  PRIMARY KEY(cpf_medico, cpf_paciente, data_consulta, cod_enfermidade)
 );
 
 CREATE TABLE consulta_enfermidade (
-  crm varchar(20),
-  cpf varchar(11),
+  cpf_medico varchar(11),
+  cpf_paciente varchar(11),
   data_consulta timestamp WITH TIME ZONE,
   cod_enfermidade integer REFERENCES enfermidade(cod_enfermidade),
-  FOREIGN KEY(crm, cpf, data_consulta) REFERENCES consulta(crm, cpf, data_consulta),
-  PRIMARY KEY(crm, cpf, data_consulta, cod_enfermidade)
+  FOREIGN KEY(cpf_medico, cpf_paciente, data_consulta)
+    REFERENCES consulta(cpf_medico, cpf_paciente, data_consulta),
+  PRIMARY KEY(cpf_medico, cpf_medico, data_consulta, cod_enfermidade)
 );
 
 CREATE TABLE anamnese_remedio (
-  crm varchar(20),
-  cpf varchar(11),
+  cpf_medico varchar(11),
+  cpf_paciente varchar(11),
   data_consulta timestamp WITH TIME ZONE,
   cod_remedio integer REFERENCES remedio(cod_remedio),
-  FOREIGN KEY(crm, cpf, data_consulta) REFERENCES consulta(crm, cpf, data_consulta),
-  PRIMARY KEY(crm, cpf, data_consulta, cod_remedio)
+  FOREIGN KEY(cpf_medico, cpf_paciente, data_consulta)
+    REFERENCES consulta(cpf_medico, cpf_paciente, data_consulta),
+  PRIMARY KEY(cpf_medico, cpf_paciente, data_consulta, cod_remedio)
 );
 
 CREATE TABLE enfermidade_remedio (
@@ -136,40 +162,43 @@ CREATE TABLE enfermidade_remedio (
 );
 
 CREATE TABLE consulta_procedimento (
-  cod_procedimento integer PRIMARY KEY REFERENCES procedimento(cod_procedimento),
-  crm varchar(20) NOT NULL,
-  cpf varchar(11) NOT NULL,
-  data_consulta timestamp WITH TIME ZONE NOT NULL,
-  FOREIGN KEY(crm, cpf, data_consulta) REFERENCES consulta(crm, cpf, data_consulta)
+  cod_procedimento integer REFERENCES procedimento(cod_procedimento),
+  cpf_medico varchar(11),
+  cpf_paciente varchar(11),
+  data_consulta timestamp WITH TIME ZONE,
+  FOREIGN KEY(cpf_medico, cpf_paciente, data_consulta)
+    REFERENCES consulta(cpf_medico, cpf_paciente, data_consulta),
+  PRIMARY KEY(cod_procedimento, cpf_medico, cpf_paciente, data_consulta)
 );
 
 CREATE TABLE profissional_procedimento (
-  matricula varchar(20) REFERENCES matricula_area(matricula),
+  cpf varchar(11) REFERENCES profissional_saude(cpf),
   cod_procedimento integer REFERENCES procedimento(cod_procedimento),
   PRIMARY KEY(matricula, cod_procedimento)
 );
 
 CREATE TABLE internacao_remedio (
-  crm varchar(20),
-  cpf varchar(11),
+  cpf_medico varchar(11),
+  cpf_paciente varchar(11),
   data_consulta timestamp WITH TIME ZONE,
   data_entrada timestamp WITH TIME ZONE,
-  cod_remedio integer REFERENCES remedio(cod_remedio) NOT NULL,
-  matricula varchar(20) REFERENCES matricula_area(matricula) NOT NULL,
+  cod_remedio integer REFERENCES remedio(cod_remedio),
+  data_administracao timestamp WITH TIME ZONE,
+  cpf_profissional varchar(11) REFERENCES profissional_saude(cpf) NOT NULL,
   dosagem varchar(20) NOT NULL,
-  data_administracao timestamp WITH TIME ZONE NOT NULL,
-  FOREIGN KEY(crm, cpf, data_consulta, data_entrada)
-    REFERENCES internacao(crm, cpf, data_consulta, data_entrada),
-  PRIMARY KEY(crm, cpf, data_consulta, data_entrada)
+  FOREIGN KEY(cpf_medico, cpf_paciente, data_consulta, data_entrada)
+    REFERENCES internacao(cpf_medico, cpf_paciente, data_consulta, data_entrada),
+  PRIMARY KEY(cpf_medico, cpf_paciente, data_consulta,
+    data_entrada, cod_remedio, data_administracao)
 );
 
 CREATE TABLE internacao_cirurgia (
-  crm varchar(20),
+  cpf_medico varchar(11),
   cpf varchar(11),
   data_consulta timestamp WITH TIME ZONE,
   data_entrada timestamp WITH TIME ZONE,
   cod_procedimento integer REFERENCES cirurgia(cod_procedimento),
-  FOREIGN KEY(crm, cpf, data_consulta, data_entrada)
-    REFERENCES internacao(crm, cpf, data_consulta, data_entrada),
-  PRIMARY KEY(crm, cpf, data_consulta, data_entrada)
+  FOREIGN KEY(cpf_medico, cpf_paciente, data_consulta, data_entrada)
+    REFERENCES internacao(cpf_medico, cpf_paciente, data_consulta, data_entrada),
+  PRIMARY KEY(cpf_medico, cpf_paciente, data_consulta, data_entrada, cod_procedimento)
 );
