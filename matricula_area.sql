22c797a6594b15a7484861b39a3c0f070618252f
Leila <zwanziger@live.com>
2017-05-20 23:56:44 -0300

    Inserts de matriculas

---
+++ matricula_area.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.matricula_area VALUES ('1245', 'Enfermagem');
+INSERT INTO public.matricula_area VALUES ('3534', 'Enfermagem');
+INSERT INTO public.matricula_area VALUES ('3456', 'Enfermagem');
+INSERT INTO public.matricula_area VALUES ('6931', 'Enfermagem');
+INSERT INTO public.matricula_area VALUES ('3859', 'Enfermagem');
+INSERT INTO public.matricula_area VALUES ('9583', 'Enfermagem');
+INSERT INTO public.matricula_area VALUES ('7967', 'Radiologia');
+INSERT INTO public.matricula_area VALUES ('9486', 'Radiologia');
+INSERT INTO public.matricula_area VALUES ('8472', 'Radiologia');
+INSERT INTO public.matricula_area VALUES ('5636', 'Cirurgia');
+INSERT INTO public.matricula_area VALUES ('4565', 'Clinica-Geral');
+INSERT INTO public.matricula_area VALUES ('5798', 'Ortopedia');
+INSERT INTO public.matricula_area VALUES ('3457', 'Cardiologia');
+INSERT INTO public.matricula_area VALUES ('4564', 'Otorrinolaringologia');
+INSERT INTO public.matricula_area VALUES ('4224', 'Pediatria');
