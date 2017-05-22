e0f7ce1a84703f1514fd34c6ce3947cfe03a408f
Leila <zwanziger@live.com>
2017-05-22 00:03:37 -0300

    Insert descrição dos remedios

---
+++ descricao_remedio.sql
@@ -0,0 +1,17 @@
+﻿INSERT INTO public.descricao_remedio(
+            cod_remedio, indicacao, dosagem_maxima)
+    VALUES (1, 'Otite externa bacteriana', '-');
+INSERT INTO public.descricao_remedio  VALUES (2, 'Antialérgico', '5mg/dia');
+INSERT INTO public.descricao_remedio  VALUES (3, 'Anticoagulante', '20mg/dia');
+INSERT INTO public.descricao_remedio  VALUES (4, 'Bloqueador beta', '100mg/dia');
+INSERT INTO public.descricao_remedio  VALUES (5, 'Soro antiofídico para cobras tipo cascavél', '-');
+INSERT INTO public.descricao_remedio  VALUES (6, 'Analgésico comprimido', '500mg/dia');
+INSERT INTO public.descricao_remedio  VALUES (7, 'Analgésico intravenoso', '240mg/dia');
+INSERT INTO public.descricao_remedio  VALUES (8, 'Indutor de contrações uterinas', '50mg/dia');
+INSERT INTO public.descricao_remedio  VALUES (9, 'Antiflamatório', '300mg/dia');
+INSERT INTO public.descricao_remedio  VALUES (10, 'Antibiotico', '500mg/dia');
+INSERT INTO public.descricao_remedio  VALUES (11, 'Colírio Antibiotico', '100mg/dia');
+INSERT INTO public.descricao_remedio  VALUES (12, 'Anestesico geral intravenoso', '150mg/dia');
+INSERT INTO public.descricao_remedio  VALUES (13, 'Anestesico geral inalavel', '8%');
+INSERT INTO public.descricao_remedio  VALUES (14, 'Broncodilatador corticóico', '800mgc/dia');
+INSERT INTO public.descricao_remedio  VALUES (15, 'Antiemético', '0,25mg/dia');
