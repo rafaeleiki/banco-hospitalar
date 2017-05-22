ecf15dd3c735352b597a57d823180bcb5bf9a897
Leila <zwanziger@live.com>
2017-05-22 00:03:44 -0300

    insert remedios

---
+++ remedio.sql
@@ -0,0 +1,17 @@
+﻿INSERT INTO public.remedio(
+            cod_remedio, nome_remedio)
+    VALUES (1, 'Otociriax');
+INSERT INTO public.remedio    VALUES (2, 'Zyxen');
+INSERT INTO public.remedio    VALUES (3, 'Rivaroxaban');
+INSERT INTO public.remedio    VALUES (4, 'Atenolol');
+INSERT INTO public.remedio    VALUES (5, 'Soro Anticrotálico');
+INSERT INTO public.remedio    VALUES (6, 'Flanax');
+INSERT INTO public.remedio    VALUES (7, 'Tylex');
+INSERT INTO public.remedio    VALUES (8, 'Misoprostol');
+INSERT INTO public.remedio    VALUES (9, 'Neosulida');
+INSERT INTO public.remedio    VALUES (10, 'Eritromicina');
+INSERT INTO public.remedio    VALUES (11, 'Tobramicina');
+INSERT INTO public.remedio    VALUES (12, 'Succinilcolina');
+INSERT INTO public.remedio    VALUES (13, 'Sevoflurano');
+INSERT INTO public.remedio    VALUES (14, 'Berotec');
+INSERT INTO public.remedio    VALUES (15, 'Onicit');
