848681c495db12984be667bfe060de455498ca06
Leila <zwanziger@live.com>
2017-05-29 00:25:03 -0300

    insert enfermidade_remedio

---
+++ seeds/enfermidade_remedio.sql
@@ -0,0 +1,27 @@
+﻿INSERT INTO enfermidade_remedio(
+            cod_enfermidade, cod_remedio)
+    VALUES (1, 1);
+INSERT INTO enfermidade_remedio VALUES (2,2);
+INSERT INTO enfermidade_remedio VALUES (4, 3);
+INSERT INTO enfermidade_remedio VALUES (4, 4);
+INSERT INTO enfermidade_remedio VALUES (5, 5);
+INSERT INTO enfermidade_remedio VALUES (6, 6);
+INSERT INTO enfermidade_remedio VALUES (7, 6);
+INSERT INTO enfermidade_remedio VALUES (9,9);
+INSERT INTO enfermidade_remedio VALUES (9,10);
+INSERT INTO enfermidade_remedio VALUES (10, 3);
+INSERT INTO enfermidade_remedio VALUES (10, 4);
+INSERT INTO enfermidade_remedio VALUES (11, 10);
+INSERT INTO enfermidade_remedio VALUES (12, 10);
+INSERT INTO enfermidade_remedio VALUES (12, 9);
+INSERT INTO enfermidade_remedio VALUES (13, 11);
+INSERT INTO enfermidade_remedio VALUES (14, 6);
+INSERT INTO enfermidade_remedio VALUES (15, 3);
+INSERT INTO enfermidade_remedio VALUES (15, 4);
+INSERT INTO enfermidade_remedio VALUES (16, 6);
+INSERT INTO enfermidade_remedio VALUES (17, 6);
+INSERT INTO enfermidade_remedio VALUES (18, 14);
+INSERT INTO enfermidade_remedio VALUES (19, 6);
+INSERT INTO enfermidade_remedio VALUES (19, 15);
+
+
