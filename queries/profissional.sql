17196365d542df1c8e1d2373c999d98cb181498f
Erick Saito <esaito@ciandt.com>
2017-05-28 21:19:14 -0300

    Profissional queries

---
+++ queries/profissional.sql
@@ -0,0 +1,24 @@
+-- Profissional
+
+-- Inserção
+    INSERT INTO pessoa VALUES (<cpf>, <nome>, <data_nascimento>, <endereco>, <telefone>, <genero>);
+    
+    INSERT INTO profissional_saude VALUES (<cpf>, <matricula>);
+
+    INSERT INTO medico VALUES (<cpf>, <crm>);
+
+    INSERT INTO enfermeiro VALUES (<cpf>, <coren>);
+
+-- Atualização
+    UPDATE pessoa set nome = <nome>, data_nascimento = <data_nascimento>, endereco = <endereco>, telefone = <telefone>, genero = <genero>
+        WHERE cpf = <cpf>;
+
+    UPDATE medico set crm = <crm> WHERE cpf = <cpf>;
+
+    UPDATE enfermeiro set coren = <coren> WHERE cpf = <cpf>;
+
+-- Consulta 
+    -- Consulta da área de um profissional 
+    SELECT ma.area as Area from matricula_area ma
+        INNER JOIN profissional_saude ps ON ps.matricula = ma.matricula
+        WHERE cpf = <cpf>
