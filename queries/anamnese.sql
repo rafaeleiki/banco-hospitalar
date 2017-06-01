8967245858b9fdf01aeb1bf3370790d1c31cbb2c
Rafael <rafaeleiki@yahoo.com.br>
2017-06-01 16:35:00 -0300

    Organizadas queries de anamnese em funções

--- queries/anamnese.sql
+++ queries/anamnese.sql
@@ -2,9 +2,32 @@
 
     -- Inserção
     INSERT INTO anamnese VALUES (<cpf_medico>, <cpf_paciente>, <data_consulta>, <pressao>, <temperatura>, <batimentos>, <queixas>, <comentarios>, <recomendacoes>);
-    
+
 
 -- Consulta
-    
+
     -- Listando anamnese
-    SELECT * FROM anamnese WHERE cpf_medico = <cpf_medico> AND cpf_paciente = <cpf_paciente> AND data_consulta BETWEEN <data_inicio> AND <data_fim>
+    CREATE OR REPLACE FUNCTION lista_anamnese_periodo(data_inicio timestamp, data_fim timestamp)
+      RETURNS SETOF anamnese
+      AS
+      'SELECT *
+        FROM anamnese
+        WHERE data_consulta BETWEEN data_inicio AND data_fim'
+      LANGUAGE 'sql';
+
+    CREATE OR REPLACE FUNCTION lista_anamnese_medico_paciente_periodo(
+        param_cpf_medico varchar, param_cpf_paciente varchar,
+        data_inicio timestamp, data_fim timestamp)
+      RETURNS SETOF anamnese
+      AS
+      'SELECT *
+        FROM lista_anamnese_periodo(data_inicio, data_fim)
+        WHERE cpf_medico = param_cpf_medico
+          AND cpf_paciente = param_cpf_paciente'
+      LANGUAGE 'sql';
+
+    CREATE OR REPLACE FUNCTION historico_anamnese_paciente(param_cpf_paciente varchar)
+      RETURNS SETOF anamnese
+      AS
+      'SELECT * FROM anamnese WHERE cpf_paciente = param_cpf_paciente'
+      LANGUAGE 'sql';
