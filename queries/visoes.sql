e91216ecfcaba152f74d20ae2a60e18d1f827cd7
Rafael <rafaeleiki@yahoo.com.br>
2017-06-02 13:30:34 -0300

    Corrige a criação múltipla de views

--- queries/visoes.sql
+++ queries/visoes.sql
@@ -1,44 +1,44 @@
--- Visoes 
+-- Visoes
 
     -- Dados completos de um profissional da saude
 
         CREATE VIEW dadosprofissional AS
         SELECT *
-        FROM pessoa NATURAL JOIN profissional_saude NATURAL JOIN matricula_area
-        
+        FROM pessoa NATURAL JOIN profissional_saude NATURAL JOIN matricula_area;
+
     -- Dados completos de um medico
 
         CREATE VIEW dadosmedico AS
         SELECT *
-        FROM pessoa NATURAL JOIN profissional_saude NATURAL JOIN matricula_area NATURAL JOIN medico
-        
+        FROM pessoa NATURAL JOIN profissional_saude NATURAL JOIN matricula_area NATURAL JOIN medico;
+
      -- Dados completos de um paciente
 
         CREATE VIEW dadospaciente AS
         SELECT *
-        FROM pessoa NATURAL JOIN paciente
-        
-    
+        FROM pessoa NATURAL JOIN paciente;
+
+
      -- Dados completos de um remedio
 
         CREATE VIEW dadosremedio AS
         SELECT *
-        FROM remedio NATURAL JOIN descricao_remedio
-        
+        FROM remedio NATURAL JOIN descricao_remedio;
+
     -- Dados completos de uma enfermidade
 
         CREATE VIEW dadosenfermidade AS
         SELECT *
-        FROM enfermidade NATURAL JOIN descricao_enfermidade
-        
+        FROM enfermidade NATURAL JOIN descricao_enfermidade;
+
      -- Dados completos de um procedimento
 
         CREATE VIEW dadosprocedimento AS
         SELECT *
-        FROM procedimento NATURAL JOIN descricao_procedimento 
-        
+        FROM procedimento NATURAL JOIN descricao_procedimento;
+
     -- Dados completos de uma cirurgia
 
         CREATE VIEW dadoscirurgia AS
         SELECT *
-        FROM dadosprocedimento NATURAL JOIN cirurgia 
+        FROM dadosprocedimento NATURAL JOIN cirurgia;
